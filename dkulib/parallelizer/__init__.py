fc4f1ae89d1f490c76e60635df2b9c0e43437b08
Niklas Muennighoff <n.muennighoff@gmail.com>
2021-07-16 15:05:49 +0200

    Add author

--- dkulib/parallelizer/__init__.py
+++ dkulib/parallelizer/__init__.py
@@ -4,7 +4,7 @@
 # For more information, see https://github.com/dataiku/dss-plugin-dkulib/tree/main/dkulib/parallelizer
 # Library version: 0.1.0
 # Last update: 2021-07-16
-# Author: Dataiku (Alex Combessie)
+# Author: Dataiku (Alex Combessie, Niklas Muennighoff)
 #########################################################
 
 from .parallelizer import DataFrameParallelizer
